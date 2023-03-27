c7f387d8f7f67bf2b5116e01d08a9fc88691897e
marcelituu <marcelsanchezroca@gmail.com>
2023-03-27 16:48:45 +0200

    Primera Version

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,36 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+     let onclick = {
+        move |_| {
+            web_sys::window()
+                .unwrap()
+                .alert_with_message("Hola")
+                .unwrap();
+        }
+    };
+
+    html! {
+        <main>
+        
+        <div style="text-align: center">
+        <h1>{ "Futuro Nombre del Proyecto de PTI" }</h1>
+        <p>{ "Introduce el usuario y la contraseña." }</p>
+        </div>
+        <div style = "margin-top: 10px; text-align: center">
+        <input type="text" placeholder="Usuario" />
+        </div>
+        <div style = "margin-top: 10px; text-align: center">
+        <input type="password" placeholder="Contraseña" />
+        </div>
+        <div style = "margin-top: 10px; text-align: center">
+            <button {onclick}>{ "login" }</button>
+        </div>
+        </main>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
