225dfe2dca042250d6dd91c9b005e9077382ce86
s4izh <sergiosanz234@gmail.com>
2023-04-06 17:56:52 +0200

    intento de docker compose

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,22 @@
-use actix_web::{web, App, HttpServer};
+use std::env;
+
 use actix_web::web::scope;
+use actix_web::{web, App, HttpServer};
 
 use handlebars::Handlebars;
 
+// use diesel::prelude::*;
+// use diesel::PgConnection;
+
 mod routes;
 use routes::frontend;
+
 use routes::mobile;
 
-pub async fn not_found(req: actix_web::HttpRequest, handlebars: web::Data<Handlebars<'_>>) -> impl actix_web::Responder {
+pub async fn not_found(
+    req: actix_web::HttpRequest,
+    handlebars: web::Data<Handlebars<'_>>,
+) -> impl actix_web::Responder {
     let data = serde_json::json!({ "url": &req.uri().to_string() });
     let body = handlebars.render("not-found", &data).unwrap();
 
@@ -21,9 +30,13 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     // set DATABASE_URL and SERVER_PORT
-    let _database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // let connection = PgConnection::establish(&database_url)
+    //     .expect(&format!("Error connecting to {}", database_url));
 
-    let backend_port: u16 = std::env::var("BACKEND_PORT")
+    // create(&mut connection);
+
+    let backend_port: u16 = env::var("BACKEND_PORT")
         .expect("BACKEND_PORT must be set")
         .parse()
         .expect("BACKEND_PORT must be a number");
@@ -41,13 +54,18 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(logger)
             .app_data(data.clone())
-            .service(scope("/api")
-                .service(scope("/frontend")
-                    .service(frontend::index)
-                    .service(frontend::get_data))
-                .service(scope("/mobile")
-                    .service(mobile::index)
-                    .service(mobile::send_data))
+            .service(
+                scope("/api")
+                    .service(
+                        scope("/frontend")
+                            .service(frontend::index)
+                            .service(frontend::get_data),
+                    )
+                    .service(
+                        scope("/mobile")
+                            .service(mobile::index)
+                            .service(mobile::send_data),
+                    ),
             )
             .default_service(web::route().to(not_found))
     })
