7cbe9e17e741fb62259136ec757f105879db438c
Ferran Fuentes <ferran.fuentes@estudiantat.upc.edu>
2023-04-17 15:34:13 +0200

    Arreglo de lloros

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -8,12 +8,12 @@ pub struct CarGeneralData {
     pub trouble_codes: Vec<String>,
     pub speed: i32,
     pub rpm: i32,
-    pub throttle f32,
-    pub engine_load f32,
-    pub engine_coolant_temp f32,
-    pub oil_temp f32, 
-    pub fuel_level f32,
-    pub fuel_consumption f32,
+    pub throttle: f32,
+    pub engine_load: f32,
+    pub engine_coolant_temp: f32,
+    pub oil_temp: f32, 
+    pub fuel_level: f32,
+    pub fuel_consumption: f32,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
