960a3360ac0f8fda5a735efb65c4b70c51de48ae
s4izh <sergiosanz234@gmail.com>
2023-05-14 18:19:34 +0200

    por si acaso

--- frontend/src/api/user_api.rs
+++ frontend/src/api/user_api.rs
@@ -127,6 +127,32 @@ pub async fn api_login_user(credentials: &str) -> Result<UserData2, String> {
     }
 }
 
+pub async fn api_user_info() -> Result<User, String> {
+    let response = match http::Request::get("http://localhost:8000/api/users/me")
+        .credentials(http::RequestCredentials::Include)
+        .send()
+        .await
+    {
+        Ok(res) => res,
+        Err(_) => return Err("Failed to make request".to_string()),
+    };
+
+    if response.status() != 200 {
+        let error_response = response.json::<ErrorResponse>().await;
+        if let Ok(error_response) = error_response {
+            return Err(error_response.message);
+        } else {
+            return Err(format!("API error: {}", response.status()));
+        }
+    }
+
+    let res_json = response.json::<UserResponse>().await;
+    match res_json {
+        Ok(data) => Ok(data.data.user),
+        Err(_) => Err("Failed to parse response".to_string()),
+    }
+}
+
 pub async fn api_certificate(credentials: &str) -> Result<String, String> {
     let response = match http::Request::post(format!("{}{}", BACKEND_URL, "/api/frontend/certificate").as_str())
         .header("Content-Type", "application/json")
